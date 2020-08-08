b949a3f243e1bb1bb16d73915abb68433b836d7e
8bithemant <hd4207577@gmail.com>
2020-08-08 12:19:23 +0530

    Css to Stay safe screen

---
+++ src/styles/StaySafeStyles.js
@@ -0,0 +1,28 @@
+import sizes from "./sizes";
+
+export default {
+  staySafe: {
+    textAlign: "center",
+
+    "& h1": {
+      marginBottom: "4.5rem",
+      [sizes.down("lg")]: {
+        margin: "4rem 0",
+      },
+
+      [sizes.down("md")]: {
+        margin: "2.5rem 0",
+      },
+    },
+  },
+  cardsBox: {
+    display: "flex",
+    flexWrap: "wrap",
+    padding: "2rem",
+    justifyContent: "space-around",
+    alignItems: "inline",
+    [sizes.down("sm")]: {
+      padding: "0",
+    },
+  },
+};
